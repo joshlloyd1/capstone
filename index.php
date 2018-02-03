7dbdb0d2a3b6c11c3c710b631b50eb632f8125ba
Ian Otenti <iotenti@gmail.com>
2018-02-03 18:51:11 -0500

    made some css changes. implemented bootsrtap navbar stuff

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 include_once ("assets/header.html");
 include_once("assets/functions.php");
 include_once("assets/dbconnect.php");
+/*
 echo "<br><br>";
 echo specialBlock();
 echo "<br><br>";
 echo aboutusBlock();
+*/
 include_once ("assets/footer.html");
 ?>
