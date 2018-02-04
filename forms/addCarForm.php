2ce2c36fdf563bc1450bd53b8394765ad2063785
Ian Otenti <iotenti@gmail.com>
2018-02-03 23:21:46 -0500

    made a form, but it all went to shit. Can't format

---
+++ forms/addCarForm.php
@@ -0,0 +1,57 @@
+<?php
+?>
+<form>
+    <div class="form-inline">
+        <div class="row">
+            <div class="col-6">
+                <label for="txtMake">Make</label>
+                <input type="text" class="form-control" id="txtMake" placeholder="Make">
+            </div>
+
+                <div class="col-6">
+                    <label for="txtModel">Model</label>
+                    <input type="text" class="form-control" id="txtModel" placeholder="Model">
+                </div>
+
+
+        </div>
+
+    </div>
+    <div class="form-inline">
+        <label for="txtTrim">Trim</label>
+        <input type="text" class="form-control" id="txtTrim" placeholder="Trim">
+
+    <div class="form-group">
+        <label for="txtVinNum">Vin Number</label>
+        <input type="text" class="form-control" id="txtVinNum" placeholder="Vin Number">
+    </div>
+    </div>
+    <div class="form-inline">
+        <label for="txtEngine">Engine</label>
+        <input type="text" class="form-control" id="txtEngine" placeholder="Engine">
+    </div>
+    <div class="form-group">
+        <label for="txtColor">Model</label>
+        <input type="text" class="form-control" id="txtColor" placeholder="Color">
+    </div>                                  <!--
+                    <div class="form-group">
+                        <label for="txtTrim">Model</label>
+                        <input type="text" class="form-control" id="txtTrim" placeholder="Trim">
+                    </div>
+                        <div class="form-group">
+                            <label for="txtTrim">Model</label>
+                            <input type="text" class="form-control" id="txtTrim" placeholder="Trim">
+                        </div> !-->
+    <div class="form-group">
+        <label for="txtDescription">Description</label>
+        <input type="text" class="form-control" id="txtDescription" placeholder="Description">
+    </div>
+    <div class="form-group">
+        <label for="txtPrice">Price</label>
+        <input type="text" class="form-control" id="txtPrice" placeholder="Price">
+    </div>
+    <div class="form-group">
+        <label for="txtMileage">Mileage</label>
+        <input type="text" class="form-control" id="txtMileage" placeholder="Mileage">
+    </div>
+</form>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include_once("assets/dbconnect.php");
 
 <h1>Index</h1>
 <?php
+include_once("forms/addCarForm.php");
 /*
 echo "<br><br>";
 echo specialBlock();
