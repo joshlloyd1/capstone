d53f9c976c835618c90c3b3a0b3df240e985a609
Ian Otenti <iotenti@gmail.com>
2018-02-04 14:48:05 -0500

    finished new car form

--- forms/addCarForm.php
+++ forms/addCarForm.php
@@ -1,25 +1,46 @@
 <?php
+/*
+ * dropdowns are messed up. Not sure why.
+ */
 ?>
 <form>
     <div class="container">
-        <div class="col-lg-4">
-            
-        </div>
-        <div class="form-group">
-            <label for="dropDownVendor">Vendor</label>
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Select Vendor...
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Vendor 1</a></br>
-                    <a class="dropdown-item" href="#">Vendor 2</a></br>
-                    <a class="dropdown-item" href="#">Vendor 3</a>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="dropdown show dropDownLoc">
+                    <button class="btn btn-secondary dropdown-toggle backEndMenuDropDown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu...
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Add New Inventory</a></br>
+                        <a class="dropdown-item" href="#">Edit A Record</a></br>
+                        <a class="dropdown-item" href="#">Delete A Record</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label for="dropDownVendor">Vendor</label>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Select Vendor...
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Vendor 1</a></br>
+                            <a class="dropdown-item" href="#">Vendor 2</a></br>
+                            <a class="dropdown-item" href="#">Vendor 3</a>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-lg-4">
+            </div>
         </div>
-
         <div class="row">
+            <div class="col-lg-4">
+
+            </div>
             <div class="col-lg-4">
                 <div class="form-group">
                     <label for="txtMake">Make</label>
@@ -32,20 +53,27 @@
                     <input type="text" class="form-control" id="txtColor" placeholder="Color">
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="form-group">
-                    <label for="txtPrice">Price</label>
-                    <input type="text" class="form-control" id="txtPrice" placeholder="Price">
-                </div>
-
-            </div>
         </div>
         <div class="row">
+            <div class="col-lg-4">
+
+            </div>
             <div class="col-lg-4">
                 <div class="form-group">
                     <label for="txtTrim">Trim</label>
                     <input type="text" class="form-control" id="txtTrim" placeholder="Trim">
                 </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label for="txtPrice">Price</label>
+                    <input type="text" class="form-control" id="txtPrice" placeholder="Price">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+
             </div>
             <div class="col-lg-4">
                 <div class="form-group">
@@ -55,40 +83,85 @@
             </div>
             <div class="col-lg-4">
                 <div class="form-group">
+                    <label for="txtVinNum">Vin Number</label>
+                    <input type="text" class="form-control" id="txtVinNum" placeholder="Vin Number">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+            </div>
+            <div class="col-lg-4">
+                <div class="form-group">
                     <label for="txtMileage">Mileage</label>
                     <input type="text" class="form-control" id="txtMileage" placeholder="Mileage">
                 </div>
             </div>
-            <div clas="row">
-                <div class="col-lg-6">
-
-                </div>
-                <div class="col-lg-6">
-
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label for="txtColor">Color</label>
+                    <input type="text" class="form-control" id="txtColor" placeholder="Color">
                 </div>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+            </div>
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label for="dropDownVendor">Fuel Type</label>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Fuel Type...
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">fuel 1</a></br>
+                            <a class="dropdown-item" href="#">fuel 2</a></br>
+                            <a class="dropdown-item" href="#">fuel 3</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label for="fileUpImage">Images</label>
+                    <input type="file" class="form-control-file" id="fileUpImage" aria-describedby="fileHelp">
+                    <small id="fileHelp" class="form-text text-muted">Upload an image</small>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+                <div class="form-group">
+                    <label for="txtDescription">Description</label>
+                    <textarea class="form-control" rows="5" id="txtDescription" placeholder="Description"></textarea>
+                </div>
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+                <button type="submit" class="btn btn-primary">Add Car</button>
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+
 
 
 
 </form>
-</div>
-<div class="form-group">
-    <label for="txtVinNum">Vin Number</label>
-    <input type="text" class="form-control" id="txtVinNum" placeholder="Vin Number">
-</div>
 
 
-                           <!--
-                    <div class="form-group">
-                        <label for="txtTrim">Model</label>
-                        <input type="text" class="form-control" id="txtTrim" placeholder="Trim">
-                    </div>
-                        <div class="form-group">
-                            <label for="txtTrim">Model</label>
-                            <input type="text" class="form-control" id="txtTrim" placeholder="Trim">
-                        </div> !-->
-<div class="form-group">
-    <label for="txtDescription">Description</label>
-    <input type="text" class="form-control" id="txtDescription" placeholder="Description">
-</div>
+
+
 
