7feafa032f54171b185442201a8f27f896fc6f4b
Ian Otenti <iotenti@gmail.com>
2018-02-06 19:17:03 -0500

    fixed the navbar. Now using bootstrap 4. Looks nice

--- assets/adminHeader.php
+++ assets/adminHeader.php
@@ -10,30 +10,39 @@
     <title>Used Cars of New England Tech</title>
 </head>
 <body>
-<div class="container">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="navbar-header">
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li><a class="navbar-brand" href="/htdocs/capstone/index.php">Used Cars of New England Tech</a></li>
-                    <li><a href="/htdocs/capstone/assets/adminViewInventory.php">Inventory</a></li>
-                    <li><a href="/htdocs/capstone/assets/adminEditInventory.php">Edit Inventory</a></li>
-                    <li><a href="/htdocs/capstone/assets/adminAppointments.php">Appointments</a></li>
-                    <li><a href="/htdocs/capstone/assets/adminVendors.php">Vendors</a></li>
-                    <li><a href="/htdocs/capstone/assets/adminMyAccount.php">My Account</a></li>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="/htdocs/capstone/index.php">Used Cars of New England Tech</a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/htdocs/capstone/assets/adminViewInventory.php">Inventory</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/htdocs/capstone/assets/adminAppointments.php">Appointments</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/htdocs/capstone/assets/adminVendors.php">Vendors</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/htdocs/capstone/assets/adminMyAccount.php">My Account</a>
+                </li>
+            </ul>
+            <div>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/htdocs/capstone/index.php">Log Out</a> <!--- LOG OUT JUST RETURNING TO INDEX ATM !-->
+                    </li>
                 </ul>
             </div>
-        </div>
-        <div class="login">
-            <a href="/htdocs/capstone/index.php">Log Out</a> <!--- LOG OUT JUST RETURNING TO INDEX ATM !-->
-        </div>
-        <div class="searchBar">
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="text" placeholder="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
-</div>
-<section>
-    <div class="container body-content" style="margin-top:90px">
+    <section>
+        <div class="container body-content" style="margin-top:90px">
